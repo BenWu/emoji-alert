d435a9231eeaa4361290c607b248bf4835e9e2f3
Benjamin Wu <benjaminwu124@gmail.com>
2020-04-24 00:02:57 -0400

    Add cloud sql/function support

--- emoji_alert/alert.py
+++ emoji_alert/alert.py
@@ -4,15 +4,30 @@ import requests
 import sqlalchemy
 from sqlalchemy import MetaData, Table
 
-if __name__ == '__main__':
+
+def main(req):
     username = os.environ['PG_USER']
     password = os.environ['PG_PASS']
     db_name = os.environ['DB_NAME']
     api_key = os.environ['API_KEY']
+    in_cloud = 'IN_CLOUD' in os.environ
 
-    connection_string = (f'postgresql+psycopg2://{username}:{password}'
-                         f'@127.0.0.1:5432/{db_name}?sslmode=disable')
-    db = sqlalchemy.create_engine(connection_string)
+    if not in_cloud:
+        connection_string = (f'postgresql+psycopg2://{username}:{password}'
+                             f'@127.0.0.1:5432/{db_name}?sslmode=disable')
+        db = sqlalchemy.create_engine(connection_string)
+    else:
+        connection_name = os.environ['CONNECTION_NAME']
+
+        db = sqlalchemy.create_engine(
+            sqlalchemy.engine.url.URL(
+                drivername='postgresql+psycopg2',
+                username=username,
+                password=password,
+                database=db_name,
+                host=f'/cloudsql/{connection_name}',
+            )
+        )
 
     with db.connect() as conn:
         emoji_table = Table('emojis', MetaData(), autoload=True, autoload_with=db)
@@ -33,7 +48,7 @@ if __name__ == '__main__':
         emojis_to_update = {
             name: url for name, url
             in emojis.items() if name in existing_emojis
-            and existing_emojis[name] != url
+                                 and existing_emojis[name] != url
         }
         for name, url in emojis_to_update.items():
             conn.execute(
@@ -46,3 +61,9 @@ if __name__ == '__main__':
         ]
         if len(new_emojis) > 0:
             conn.execute(emoji_table.insert(values=new_emojis))
+
+        print(new_emojis)
+
+
+if __name__ == '__main__':
+    main(None)
